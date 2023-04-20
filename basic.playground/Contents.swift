9db4a0ef150dc8a8f9280997c85b176888c85a5f
Анна Болотова <annabolotova@MacBook-Pro-Anna.local>
2023-04-20 16:15:56 +0300

    add a new file. code learninf string

---
+++ basic.playground/Contents.swift
@@ -0,0 +1,14 @@
+var greeting = "Hello, "
+var name = "Helen"
+var message = greeting + name
+var age = 34
+
+// Method to convert a string to upper case
+message.uppercased()
+// Method to convert a string to lower case
+message.lowercased()
+// Count the number of characters og rhe string
+message.count
+
+//String Interpolations
+var accost = "\(greeting) my name is \(name). I'm \(age) years old"
