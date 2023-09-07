6ce68cf06b93d714ac1120a236e736d75703e5ae
Анна Болотова <annabolotova@MacBook-Pro-Anna.local>
2023-09-08 00:10:46 +0300

    add new changes

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("hi, Hanna")
+            Text("Turtle Rock")
+                .font(.title)
+                .foregroundColor(.black)
         }
         .padding()
     }
